5cac859788ce79bb3dc343dcef157ac7fa309534
Ramkrishna <ramkrishna7710@gmail.com>
2026-05-29 11:24:16 +0530

    solving port issue

--- Backend/server.js
+++ Backend/server.js
@@ -72,6 +72,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Running ${PORT}`);
-});
+app.listen(
+  PORT,
+  "0.0.0.0",
+  () => {
+    console.log(`Running ${PORT}`);
+  },
+);
