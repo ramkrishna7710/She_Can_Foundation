9a3505b1a3401e4f4fa5d4f068c8c4c16532f03f
Ramkrishna <ramkrishna7710@gmail.com>
2026-05-29 10:47:06 +0530

    updated baseUrl

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: import.meta.env.VITE_API
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API
+  // baseURL: "http://localhost:5000/api"
 });
 
 export default API;
